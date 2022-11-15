e786b4b65ea6a7a6e095d61d15c48071bb6aab5c
murrkii 3 <99413747+murrkii@users.noreply.github.com>
2022-11-15 08:58:06 +0800

    wee

---
+++ exercise06taubaniquedmarkymiguel/Exercise06taubaniquedmarkymiguel.java
@@ -0,0 +1,10 @@
+package exercise06taubaniquedmarkymiguel;
+
+public class Exercise06taubaniquedmarkymiguel {
+
+    public static void main(String[] args) {
+        FireType f = new FireType("Sumn", 75, 25);
+        //continue at haurme
+    }
+    
+}
